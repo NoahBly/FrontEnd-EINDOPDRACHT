aa86ec51f555ca9bf1c39207f0fc28a22dd6f7ea
NoahBly <raz_b4@hotmail.com>
2023-02-15 16:18:26 +0100

    deze is beter dan vorige, profiel beter

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,16 +5,16 @@ import {Link} from "react-router-dom";
 
 function Profile() {
     const [profile, setProfile] = useState({});
-    const { setProfilepostsfunction } = useContext(AuthContext);
-    const{userDetails} = useContext(AuthContext);
+    const {setProfilepostsfunction} = useContext(AuthContext);
+    const {userDetails} = useContext(AuthContext);
 
-   useEffect(() => {
+    useEffect(() => {
         console.log(userDetails.id);
 
 
         async function fetchData(userid) {
             try {
-                const { data } = await axios.get(`http://localhost:8083/profiles/user/${userid}`);
+                const {data} = await axios.get(`http://localhost:8083/profiles/user/${userid}`);
                 console.log(data);
                 setProfile(data);
                 setProfilepostsfunction(data.posts);
@@ -30,36 +30,40 @@ function Profile() {
 
 
     return (
+
         <section className="poke-card">
+            {profile &&
+            <>
+                <h2>{profile.name}</h2>
+                <img
+                    alt="Afbeelding profile"
+                    src={profile.profileimage}
+                />
+                <p><strong>Friendlist: </strong>{profile.friendlist && <p>{profile.friendlist.length}</p>}</p>
+                <p><strong>Followerslist: </strong>{profile.friendlist && <p>{profile.followerslist.length}</p>}</p>
+                <p><strong>Followingslist: </strong>{profile.friendlist && <p>{profile.followinglist.length}</p>}</p>
+                <p><strong>Bio: </strong>{profile.bioinformation}</p>
+                <p><strong>Posts: </strong></p>
+                {profile.posts &&
+                    <ul>
+                        {profile.posts.map((post) => {
+                            return (
+                                <li key={`${post.id}-${post.name}`}>
+                                    <Link to={`/profile/post/${post.id}`}>
+                                        {post.title}
+                                    </Link>
+                                    {post.name}
+                                    {post.imagevideo}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                }
 
-                <>
-                    <h2>{profile.name}</h2>
-                    <img
-                        alt="Afbeelding profile"
-                        src={profile.profileimage}
-                    />
-                    <p><strong>Friendlist: </strong>{profile.friendlist.length}</p>
-                    <p><strong>Followerslist: </strong>{profile.followerslist.length}</p>
-                    <p><strong>Followingslist: </strong>{profile.followinglist.length}</p>
-                    <p><strong>Bio: </strong>{profile.bioinformation}</p>
-                    <p><strong>Posts: </strong></p>
-                    {/*<ul>*/}
-                    {/*    {profile.posts.map((post) => {*/}
-                    {/*        return (*/}
-                    {/*            <li key={`${post.id}-${post.name}`}>*/}
-                    {/*                <Link to={`/profile/post/${post.id}`}>*/}
-                    {/*                    {post.title}*/}
-                    {/*                </Link>*/}
-                    {/*                {post.name}*/}
-                    {/*                {post.imagevideo}*/}
-                    {/*            </li>*/}
-                    {/*        )*/}
-                    {/*    })}*/}
-                    {/*</ul>*/}
-                </>
-
+            </>
+            }
         </section>
-    );
+    )
+    ;
 }
-
 export default Profile;
