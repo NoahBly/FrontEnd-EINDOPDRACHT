a62d48ad42de8c1242b9abbd638dce0a668c42d2
NoahBly <raz_b4@hotmail.com>
2023-03-06 17:15:48 +0100

    nu pas focussen op css, srry moest nog kleine aanpassing

--- src/pages/Searchresultsprofile.js
+++ src/pages/Searchresultsprofile.js
@@ -9,7 +9,7 @@ function Searchresultsprofile() {
     const {setVisitedprofilepostsfunction} = useContext(AuthContext);
     const [profilevisited, setProfilevisited] = useState();
     //const {profile2id, setprofile2id} = useState();
-    const {visitedprofileid,setVisitedprofileidfunction} = useContext(AuthContext);
+    const {visitedprofileid, setVisitedprofileidfunction} = useContext(AuthContext);
     const [imageBlob, setImageBlob] = useState();
 
 
@@ -17,110 +17,117 @@ function Searchresultsprofile() {
         console.log(profile2Id);
 
 
-         if(!profile2Id.isEmpty) {
-             const profile2id = profile2Id;
-             setVisitedprofileidfunction(profile2Id);
+        if (!profile2Id.isEmpty) {
+            const profile2id = profile2Id;
+            setVisitedprofileidfunction(profile2Id);
 
-             async function fetchData() {
-                 try {
-                     const {data} = await axios.get(`http://localhost:8083/profiles/${profile2id}`);
-                     console.log(data);
-                     setProfilevisited(data);
-                     setVisitedprofilepostsfunction(data.posts);
-                     // setProfileidfunction(data.id);
-                 } catch (e) {
-                     console.error(e);
-                 }
-             }
-
-             fetchData();
-
-         }}, []);
-
-    useEffect(() => {
-
-
-        if(!profile2Id.isEmpty) {
-            const profilevisitedid = profile2Id;
-
-
-        async function fetchData2() {
-
+            async function fetchData() {
                 try {
-                    const response = await axios.get(`http://localhost:8083/profiles/download/${profilevisitedid}`, {
-                        responseType: "blob"
-                    });
-                    const blob = response.data;
-                    const reader = new FileReader();
-                    reader.onload = function (e) {
-                        const image = new Image();
-                        image.src = e.target.result;
-                        setImageBlob(image);
+                    const {data} = await axios.get(`http://localhost:8083/profiles/${profile2id}`);
+                    console.log(data);
+                    setProfilevisited(data);
+                    setVisitedprofilepostsfunction(data.posts);
+
+                    if (data.profileimage !== null) {
+                        fetchData2();
                     }
-                    // console.log(data);
-                    // setImageBlob(data);
-                    // const bloburl = URL.createObjectURL(data.blob);
-                    // console.log(bloburl);
-                    reader.readAsDataURL(blob);
-                    console.log(imageBlob)
-                    console.log(response);
-                    console.log(response.data);
+
+
+                    // setProfileidfunction(data.id);
                 } catch (e) {
                     console.error(e);
                 }
             }
 
-                fetchData2();
+            if (profile2id) {
+                fetchData();
+            }
+        }
+    }, []);
 
-        }}, []);
 
 
+        const profilevisitedid = profile2Id;
 
+        console.log(profilevisited);
+        async function fetchData2() {
 
-    return (
-        <section className="poke-card">
-            {profilevisited &&
-            <>
-                <h2>{profilevisited.name}</h2>
-                {imageBlob  && <img
-                    alt="Afbeelding profile"
-                    src={imageBlob.src} width="500px"
-                /> }
-
-                <p><strong>Friendlist: </strong>{profilevisited.friendlist && <p>{profilevisited.friendlist.length}</p>}</p>
-                <p><strong>Followerslist: </strong>{profilevisited.followerslist && <p>{profilevisited.followerslist.length}</p>}</p>
-                <p><strong>Followingslist: </strong>{profilevisited.followingslist && <p>{profilevisited.followingslist.length}</p>}</p>
-
-                <Link to={`/createfriendrequest/create`}>
-                  <p>  Create Friendrequest!</p>
-                </Link>
-
-                <Link to={`/createfollowrequest/create`}>
-                   <p>Create Followrequest</p>
-                </Link>
-
-                <p><strong>Bio: </strong>{profilevisited.bioinformation}</p>
-
-                <p><strong>Posts: </strong></p>
-                {profilevisited.posts &&
-                <ul>
-                    {profilevisited.posts.map((post) => {
-                        return (
-                            <li key={`${post.id}-${post.name}`}>
-                                <Link to={`/searchresultsposts/profile/${post.id}`}>
-                                    {post.name}
-                                </Link>
-                                <h2>{post.name}</h2>
-                            </li>
-                        )
-                    })}
-                </ul>
+            try {
+                const response = await axios.get(`http://localhost:8083/profiles/download/${profilevisitedid}`, {
+                    responseType: "blob"
+                });
+                const blob = response.data;
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    const image = new Image();
+                    image.src = e.target.result;
+                    setImageBlob(image);
                 }
-            </>
+                // console.log(data);
+                // setImageBlob(data);
+                // const bloburl = URL.createObjectURL(data.blob);
+                // console.log(bloburl);
+                reader.readAsDataURL(blob);
+                console.log(imageBlob)
+                console.log(response);
+                console.log(response.data);
+            } catch (e) {
+                console.error(e);
             }
 
-        </section>
-    );
-}
+
+
+        }
+
+
+        return (
+            <section className="poke-card">
+                {profilevisited &&
+                    <>
+                        <h2>{profilevisited.name}</h2>
+                        {imageBlob && <img
+                            alt="Afbeelding profile"
+                            src={imageBlob.src} width="500px"
+                        />}
+
+                        <p><strong>Friendlist: </strong>{profilevisited.friendlist &&
+                            <p>{profilevisited.friendlist.length}</p>}</p>
+                        <p><strong>Followerslist: </strong>{profilevisited.followerslist &&
+                            <p>{profilevisited.followerslist.length}</p>}</p>
+                        <p><strong>Followingslist: </strong>{profilevisited.followingslist &&
+                            <p>{profilevisited.followingslist.length}</p>}</p>
+
+                        <Link to={`/createfriendrequest/create`}>
+                            <p> Create Friendrequest!</p>
+                        </Link>
+
+                        <Link to={`/createfollowrequest/create`}>
+                            <p>Create Followrequest</p>
+                        </Link>
+
+                        <p><strong>Bio: </strong>{profilevisited.bioinformation}</p>
+
+                        <p><strong>Posts: </strong></p>
+                        {profilevisited.posts &&
+                            <ul>
+                                {profilevisited.posts.map((post) => {
+                                    return (
+                                        <li key={`${post.id}-${post.name}`}>
+                                            <Link to={`/searchresultsposts/profile/${post.id}`}>
+                                                {post.name}
+                                            </Link>
+                                            <h2>{post.name}</h2>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        }
+                    </>
+                }
+
+            </section>
+        );
+    }
+
 
 export default Searchresultsprofile;
